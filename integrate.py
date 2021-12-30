51e90445145c8b3563e0062a89928cbc743f5e78
Alec <alecautry20181@gmail.com>
2021-12-30 09:21:36 -0600

    added more code

--- integrate.py
+++ integrate.py
@@ -1,12 +1,29 @@
 #import stuff here
 
 
-#create simple integrate function (Ax^n = A*n/(n+1)x^n+1 + C)
+#create simple integrate function (Ax^n = A/(n+1)x^n+1 + C)
 def integrate_simple(integrand):
     #do simple integral
     integral = ""
-    integrandSplit = integrand.split('x')
-    integral = integrandSplit[0]
+    coeffeciant = ""
+    power = ""
+
+    #solves just x
+    if(integrand == "x"):
+        integral = ".5x^2"
+        return integral
+    #solves just x^n
+    
+    #solves just Ax
+
+
+    #solves in the form of integral(Ax^n)
+    integrandSplit = integrand.split('x') #split the values for A and ^n
+    coeffeciant = integrandSplit[0]
+    for item in integrandSplit[1]:
+        if(item != "^"):
+            power += item
+    integral = str(string_to_math(coeffeciant)/(string_to_math(power)+1))+"x^" + str(string_to_math(power)+1) + " + C"
     return integral
 
 def integrate_trig(integrand):
@@ -23,7 +40,7 @@ def integrate_letu(integrand):
 
 
 
-def string_to_math(function):
+def string_to_math(function): #string return value
     #take the string function and do the math it cotains EX: (6/7 + 3)
     value = eval(function) #careful doing the eval as it executes in python code found in the string
     return value
@@ -31,6 +48,5 @@ def string_to_math(function):
 #MAIN FUNCTION
 
 myIntegrand = "6x^7"
-AbigailsNum = 34
 myIntegral = integrate_simple(myIntegrand)
 print(myIntegral)
